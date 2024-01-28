74885b4b66f9576305b59c8f02f0c1b65f718202
Your Name <119500341+edinsonRmz@users.noreply.github.com>
2024-01-28 15:16:42 -0500

    ....

--- src/Componentes/Descripcion.jsx
+++ src/Componentes/Descripcion.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './../css/Descripcion.css';
-import HTML from '.';
+import HTML from './../assets/img/html5-original-wordmark.svg';
 import CSS from './../assets/css3-plain.svg';
 import JAVASCRIPT from './../Img/javascript-plain.svg';
 import BOOTSTRAP from './../Img/bootstrap-plain.svg';
